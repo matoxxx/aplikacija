2b929fcea657ae67fac94b2d59f76b78d7027ed3
blaz1 <arti.iluminati@gmail.com>
2014-04-18 15:01:45 +0200

    ....

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,11 +7,34 @@ var betAfriendControllers = angular.module('betAfriendControllers', []);
 betAfriendControllers.controller('BetListCtrl', ['$scope', '$http',
   function($scope, $http) {
     $http.get('json/bets.json').success(function(data) {
-      $scope.bets = data;
     });
+    $scope.bets = [
+      {'name': 'kdo je bolši',
+       'description': 'Fast just got faster with Nexus S.', 
+       'author': 'john doe',
+       'categories': 'Friends',
+       'Status': 'In progress',
+       'reward': '10€',
+       'end date': '12.2.2015'},
+      {'name': 'drinking beer',
+       'description': 'This bet is about drinking 9 cups of beer in about half an hour, without throwing up.', 
+       'author': 'janez novak',
+       'categories': 'Fun&Party',
+       'Status': 'Finished',
+       'reward': '15€',
+       'end date': '17.4.2014'},
+      {'name': 'sporting',
+       'description': 'Who will be the faster in 60m race.', 
+       'author': 'športni navdušenec',
+       'categories': 'Sport',
+       'Status': 'Pending',
+       'reward': '5€',
+       'end date': '17.4.2014'},
 
-    $scope.orderProp = 'age';
+    ];
+
     loadTagCategories("categoriesTags");
+    $('.tooltip1').tooltip();
   }]);
 
 betAfriendControllers.controller('CategoriesListCtrl', ['$scope', '$http',
