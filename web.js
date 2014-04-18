5336496a1cb0f2d11fc835134f5e687a72ce1bab
matox <mh3500@student.uni-lj.si>
2014-04-18 21:08:57 +0200

    New Procfile

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname+'/app'));
+app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/bower_components'));
 
 var port =  process.env.PORT || 8000;
 app.listen(port);
