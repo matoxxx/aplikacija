1451bb58891c776c2526630925164f4d2c6e55a9
blaz1 <arti.iluminati@gmail.com>
2014-04-18 16:15:49 +0200

    almost finished browse bets page, added connection with json files

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,8 +7,19 @@ var betAfriendControllers = angular.module('betAfriendControllers', []);
 betAfriendControllers.controller('BetListCtrl', ['$scope', '$http',
   function($scope, $http) {
     $http.get('json/bets.json').success(function(data) {
+      $scope.bets = data;
     });
-    $scope.bets = [
+    $http.get('json/categories.json').success(function(data) {
+      $scope.categories = data;
+    });
+    $http.get('json/bets-users.json').success(function(data) {
+      $scope.betsUsers = data;
+    });
+    $http.get('json/users.json').success(function(data) {
+      $scope.users = data;
+    });
+      $('.tooltip1').tooltip();
+    /*$scope.bets = [
       {'name': 'kdo je bolši',
        'description': 'Fast just got faster with Nexus S.', 
        'author': 'john doe',
@@ -31,14 +42,17 @@ betAfriendControllers.controller('BetListCtrl', ['$scope', '$http',
        'reward': '5€',
        'end date': '17.4.2014'},
 
-    ];
+    ];*/
 
     loadTagCategories("categoriesTags");
-<<<<<<< HEAD
-    $('.tooltip1').tooltip();
-=======
+
+    //$(document).ready(function() {
+      $('.tooltip1').tooltip();
+    //}
+
     $(':checkbox').checkbox();
->>>>>>> matox/angular
+      $('.tooltip1').tooltip();
+
   }]);
 
 betAfriendControllers.controller('CategoriesListCtrl', ['$scope', '$http',
