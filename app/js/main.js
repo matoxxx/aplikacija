e01507271bc7e72a8841e65d9ab47577afee26f0
matox <mh3500@student.uni-lj.si>
2014-05-13 07:00:09 +0200

    design updates

--- app/js/main.js
+++ app/js/main.js
@@ -10,3 +10,12 @@ function loadTagCategories(id) {
 	});
 }
 
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+    parallax();
+});
