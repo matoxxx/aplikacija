4b62b1b757e748fc1e5e3e5ad0912871923f051d
matox <mh3500@student.uni-lj.si>
2014-05-28 12:28:28 +0200

    disqus comments added, new location route added

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -33,24 +33,63 @@ betAfriendControllers.controller('BrowseBetsController', ['$scope', '$http', '$f
     var betsSource = new Firebase("https://dazzling-fire-5750.firebaseio.com/bets/");
     var usersSource = new Firebase("https://dazzling-fire-5750.firebaseio.com/users/");
     $scope.bets = $firebase(betsSource);
-    $scope.bets = $firebase(usersSource);
+    $scope.users = $firebase(usersSource);
     $scope.orderProp = 'age';
 }]);
 
 /* CREATE BET CONTROLLER */
-betAfriendControllers.controller('CreateBetController', ['$scope', '$http', function($scope, $firebase, $http) {
-    $http.get('json/categories.json').success(function(data) {
-        $scope.categories = data;
-    });
+betAfriendControllers.controller('CreateBetController', ['$scope', '$firebase', '$http', function($scope, $firebase, $http) {
+    // $http.get('json/categories.json').success(function(data) {
+    //     $scope.categories = data;
+    // });
 
-    $scope.orderProp = 'age';
+    // $scope.orderProp = 'age';
 }]);
 
 /* BET DETAIL CONTROLLER */
-betAfriendControllers.controller('BetDetailController', ['$scope', '$firebase', '$routeParams', '$http', function($scope, $firebase, $routeParams, $http) {
-    var betsSource = new Firebase("https://dazzling-fire-5750.firebaseio.com/bets/" + $routeParams.betId);
-    $scope.bet = $firebase(betsSource);
-}]);
+betAfriendControllers.controller('BetDetailController', ['$scope', '$firebase', '$routeParams', '$http', '$log', function($scope, $firebase, $routeParams, $http, $log) {
+    var betSource = new Firebase("https://dazzling-fire-5750.firebaseio.com/bets/" + $routeParams.betId);
+    $scope.bet = $firebase(betSource);
+}]).directive('dirDisqus', function($window) {
+    return {
+        restrict: 'E',
+        scope: {
+            disqus_shortname: '@disqusShortname',
+            disqus_identifier: '@disqusIdentifier',
+            disqus_title: '@disqusTitle',
+            disqus_url: '@disqusUrl',
+            disqus_category_id: '@disqusCategoryId',
+            disqus_disable_mobile: '@disqusDisableMobile',
+            readyToBind: "@"
+        },
+        template: '<div id="disqus_thread"></div><a href="http://disqus.com" class="dsq-brlink">comments powered by<span class="logo-disqus">Disqus</span></a>',
+        link: function(scope) {
+
+            scope.$watch("readyToBind", function(isReady) {
+
+                // If the directive has been called without the 'ready-to-bind' attribute, we
+                // set the default to "true" so that Disqus will be loaded straight away.
+                if ( !angular.isDefined( isReady ) ) {
+                    isReady = "true";
+                }
+                if (scope.$eval(isReady)) {
+                    // put the config variables into separate global vars so that the Disqus script can see them
+                    $window.disqus_shortname = scope.disqus_shortname;
+                    $window.disqus_identifier = scope.disqus_identifier;
+                    $window.disqus_title = scope.disqus_title;
+                    $window.disqus_url = scope.disqus_url;
+                    $window.disqus_category_id = scope.disqus_category_id;
+                    $window.disqus_disable_mobile = scope.disqus_disable_mobile;
+
+                    // get the remote Disqus script and insert it into the DOM
+                    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                    dsq.src = '//' + scope.disqus_shortname + '.disqus.com/embed.js';
+                    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                }
+            });
+        }
+    }
+});
 
 /* USER DETAIL CONTROLLER */
 betAfriendControllers.controller('UserDetailController', ['$scope', '$firebase', '$routeParams', '$http', function($scope, $firebase, $routeParams, $http) {
